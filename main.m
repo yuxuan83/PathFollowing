30262d9bdfea713bc00f8f2c16f9b9e0e4c7925e
ChangYuHsuan <changyh1994@gmail.com>
2019-04-08 15:36:33 -0400

    add main file

---
+++ main.m
@@ -0,0 +1,27 @@
+%% Define paramenters
+L = 3; % m
+b = 1.5; % m
+dt = 0.1; % sampling period [s]
+T = 6; % total time span [s]
+
+%% 1. generate reference trajectory
+% given waypoints solve open loop characteristic and corresponding input
+% function (steering angle and acceleration)
+
+%% 2. interpolation reference trajectroy
+% vq = interp1(x, v, xq) 
+
+%% 3. linearization about the reference trajectory
+% x(i+1) = Ai * x(i) + Bi * u(i)
+
+%% 4. MPC loop
+% define MPC paramenters and constraints
+% initial condition X0 = [x0, y0, psi0, v0]
+
+% for
+%   compute equality constraint
+%   compute inequality constraint
+%   solve qp
+%   simulate output using ode45 function
+%   record actual state x
+% end
