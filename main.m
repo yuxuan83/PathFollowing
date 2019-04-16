f30147f08f3ecda13168048cc8bd2759521c81dc
ChangYuHsuan <changyh1994@gmail.com>
2019-04-16 00:07:29 -0400

    construct state transition matrix

--- main.m
+++ main.m
@@ -7,14 +7,14 @@ dt = 0.05; % sampling period [s]
 T_max = 6; % total time span [s]
 T = 0:dt:T_max;
 
-%% 1. load and interpolate reference trajectory
+%% 1. Load and interpolate reference trajectory
 load('ref_traj.mat');
 
 U_ref = interp1(0:0.05:T_max, [U(:,1),U]', T)';
-Y_ref = interp1(0:0.05:T_max, Y', T)';
+X_ref = interp1(0:0.05:T_max, X', T)';
 
 figure(1)
-plot(Y_ref(1,:), Y_ref(2,:), '--', 'LineWidth', 1.2)
+plot(X_ref(1,:), X_ref(2,:), '--', 'LineWidth', 1.2)
 axis equal
 xlim([-0.5, 6.5])
 
@@ -29,15 +29,14 @@ ylim([-1 1]);
 grid on
 
 %% 2. MPC loop
-% define MPC paramenters and constraints
-n_pred = 10; % finite time step
+% Define MPC paramenters and constraints
+Np = 10; % finite time step
+Q = 10*eye(4);
+R = 0.2*eye(2);
+Q_blk = kron(eye(Np+1), Q);
+R_blk = kron(eye(Np), R);
 
-Ndec=3*(n_pred+1)+2*n_pred; 
-
-% initial condition X0 = [x0; y0; psi0; v0]
-x0 = [0.25; 0.25; -1; 5];
-
-% input and state contraints
+% Input and state contraints
 delta_upper = 37 * (pi/180); % [rad]
 delta_lower = -37 * (pi/180); % [rad]
 acc_upper = 1.5; % [m/s^2]
@@ -47,12 +46,50 @@ y_lower = -1.6; % [m]
 x_upper = 3; % [m]
 x_lower = -1; % [m]
 
+% Initial condition x = [x0; y0; psi0; v0], u = [delta0, acc0]
+x = [0.25; 0.25; -1; 0];
+u = [0; 0];
 
+% Iterations
+for i = 1:1
+    
+    % Construct state transition matrix
+    x_ref = X_ref(:,i:i+Np-1);
+    u_ref = U_ref(:,i:i+Np-1);
+    
+    A_cell = cell(Np,1);
+    B_cell = cell(Np,1);
+    for j = 1:Np
+        [A_cell{j}, B_cell{j}] = ltv_mdl(x_ref(:,j), u_ref(:,j), dt);
+    end
+    
+    F_cell = cell(Np+1, 1);
+    Phi_cell = cell(Np+1, Np);
+    for j = 1:Np+1
+        F_cell{j} = ones(4,4);
+        for k = 1:Np
+            if k < j
+                Phi_cell{j, k} = ones(4,2);
+            else
+                Phi_cell{j, k} = zeros(4,2);
+            end
+            
+        end
+    end
+    F = cell2mat(F_cell);
+    Phi = cell2mat(Phi_cell);
+    
+    % Inequality constraint
+    
+    
+%     H = Phi'*Q_blk*Phi + R_blk;
+    % Solve qp
+%     dU = cplexqp(H, zeros(4*(Np+1), 1), A, b);
+    % Simulate output using ode45 function
+    
+    % Record actual state x and output 
 
-% for loop
-%   compute equality constraint with linearized model (trajectory)
-%   compute inequality constraint
-%   solve qp
-%   simulate output using ode45 function
-%   record actual state x
-% end
+end
+
+%% Plot results
+
